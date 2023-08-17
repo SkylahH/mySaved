e6fe0f3d2017ca57a4e65c1612d4f3243459e188
Skylah Hatch <sky55smiles@gmail.com>
2023-08-17 14:45:25 -0400

    Initial Commit

---
+++ mySaved/mySavedApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySavedApp.swift
+//  mySaved
+//
+//  Created by scholar on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct mySavedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
